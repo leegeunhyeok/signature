e8455aded027ae7c5ab34fb78e6459809b2e854e
leegeunhyeok <dev.ghlee@gmail.com>
2021-04-12 17:38:37 +0900

    feat: add sample svg path and animating

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,50 @@
-import logo from './logo.svg';
-
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import SVGLine from './components/SVGLine';
+import { useAnimate } from './hooks/animate';
+
+// Sample path
+const d =
+  'M58.08 0C58.08 18.1895 15.157 35.7824 5.76 50.4C3.51545 53.8915 0 61.44 0 61.44C0 61.44 2.50927 57.5652 3.84 55.68C8.74496 48.7313 35.1275 30.5238 45.12 35.52C51.4037 38.6619 30.663 59.6055 34.56 53.76C45.5122 37.3317 66.5673 45.8812 80.16 39.84C88.3709 36.1907 95.1862 31.8863 102.24 26.4C104.474 24.6621 106.478 18.6384 108.48 20.64C110.983 23.1433 106.781 30.2926 116.64 32.64C129.467 35.6941 139.852 32.9902 151.2 26.88C151.598 26.6654 152.48 25.6 152.16 25.92C148.645 29.4346 166.141 34.4988 168.96 31.68';
 
 function App() {
+  const [isReady, setReadyState] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const controller = useAnimate((p) => {
+    setProgress(p);
+  }, 2000);
+
+  useEffect(() => {
+    setTimeout(() => setReadyState(true), 500);
+  }, []);
+
   return (
-    <AppContainer>
-      <AppHeader>
-        <AppLogo src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </AppHeader>
+    <AppContainer ready={isReady}>
+      <AppHeader>{progress}</AppHeader>
+      <SVGLine progress={progress * 100} color="#282c34" d={d} />
+      <button onClick={controller.current.start}>Start</button>
+      <button onClick={controller.current.stop}>Stop</button>
     </AppContainer>
   );
 }
 
 export default App;
 
-const AppContainer = styled.div`
+interface AppContainerProps {
+  ready: boolean;
+}
+
+const AppContainer = styled.div<AppContainerProps>`
+  ${(props) => props.ready && 'background-color: #fff'};
   text-align: center;
+  transition: background-color 300ms;
 `;
 
 const AppHeader = styled.div`
-  background-color: #282c34;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-const AppLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
 `;
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
+    background-color: #282c34;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
       'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
